a9bf2184aacc841bb1138db24de11115616adacb
harshit1507 <harshit1507@yahoo.com>
2021-01-11 22:33:37 +0530

    AdManager Added

---
+++ Assets/Scipts/AdManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdManager : MonoBehaviour
+{
+    string gameId = "3970371";
+    bool testMode = true;
+    private void Start() {
+        Advertisement.Initialize(gameId, testMode);
+        // Advertisement.show("video");
+    }
+    void Update()
+    {
+        // if(Input.GetKeyDown(KeyCode.E))
+        // {
+        //     if(Advertisement.IsReady("video"))
+        //     {
+        //         Advertisement.Show("video");
+        //     }
+        //     Debug.Log("E is Pressed");
+        // }
+    }
+    public void showAds()
+    {
+        Advertisement.Show("video");
+    }
+}
